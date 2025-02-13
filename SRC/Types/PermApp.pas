a5ecb4a932c59ad246bf069115a4791e0a2419f5
serge <s.borenko@gmail.com>
2025-02-13 02:05:26 +0200

    Update from XP 2

---
+++ SRC/Types/PermApp.pas
@@ -0,0 +1,34 @@
+unit PermApp;
+
+interface
+
+uses
+  Item;
+
+type
+  TPermApp = class(TItem)
+  public
+    function DatasetName: ShortString; override;
+    function DatasetFields: String; override;
+  end;
+
+implementation
+
+uses
+  TextLib;
+
+//------------------------------------------------------------------------------
+function TPermApp.DatasetName;
+begin
+  Result := 'PermApp';
+end;
+
+//------------------------------------------------------------------------------
+function TPermApp.DatasetFields: String;
+begin
+  Result := inherited DatasetFields;
+  Result := TextUtils.ConcatStr(Result, 'PermId Integer not null', ',');
+  Result := TextUtils.ConcatStr(Result, 'AppId Integer not null', ',');
+end;
+
+end.
